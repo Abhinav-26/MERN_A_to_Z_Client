ba4c921a4f3d4b87cbb9324dcba3973caba24fbe
Abhinav-26 <abhinavdubey616@gmail.com>
2022-09-07 15:35:28 +0530

    api-urls changed

--- src/components/ShowBookList.js
+++ src/components/ShowBookList.js
@@ -14,7 +14,7 @@ class ShowBookList extends Component {
 
   componentDidMount() {
     axios
-      .get('https://mernbackend.devtron.info/api/books')
+      .get('https://frontend.devtron.info/api/books')
       .then(res => {
         this.setState({
           books: res.data
